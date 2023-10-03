467e295261570f943df5e81fcbb2843c63d80d08
HaiderBajwa111 <haider.bajwa.111@gmail.com>
2023-10-03 14:56:21 -0700

    Artboard Admin_Section Prices About_Me My_Posts screen

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        custom: '1.5px 2.598076105117798px 5px rgba(119,119,119,0.1)'
+      }
+    },
+  },
+  plugins: [],
+}
